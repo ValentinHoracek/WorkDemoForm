e8073d2a29b23e6d5913be3dab82d5d0c6983cdb
Valentin Horacek <valentin.horacek@gmail.com>
2025-01-16 13:55:38 +0100

    Form init

--- WorkDemoForm/WorkDemoForm/Pages/Index.cshtml.cs
+++ WorkDemoForm/WorkDemoForm/Pages/Index.cshtml.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WorkDemoForm.Models;
 
 namespace WorkDemoForm.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public FormModel InputForm { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -15,6 +19,16 @@ namespace WorkDemoForm.Pages
         public void OnGet()
         {
 
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            return Page();
         }
     }
 }
